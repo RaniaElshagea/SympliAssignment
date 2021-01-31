dc30e4983d400c4d774d54b4909d7075988f7e7d
Rania Gamal <rania.gamal@gmail.com>
2021-01-31 19:08:04 +1100

    filling factory from assembly instead of manually adding them

--- Sympli.Infrastructure/SearchEngines/SearchEngineFactory.cs
+++ Sympli.Infrastructure/SearchEngines/SearchEngineFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Sympli.Application.Contracts;
 
@@ -6,16 +7,23 @@ namespace Sympli.InfrastructureSearchEngines
 {
     public class SearchEngineFactory: ISearchEngineFactory
     {
-        private IList<ISearchEngine> _searchEngines = new List<ISearchEngine>();
-        public SearchEngineFactory()//ISearchEngine[] searchEngines
-        {
-            _searchEngines.Add(new GoogleSearchEngine());
-            _searchEngines.Add(new BingSearchEngine());
-        }
+        private IEnumerable<ISearchEngine> searchEngines => GetSearchEngines();
 
         public ISearchEngine Create(string engineName)
         {
-            return _searchEngines.Single(s => s.WhichEngineToHandle(engineName));
+            return searchEngines.Single(s => s.WhichEngineToHandle(engineName));
+        }
+
+        private IEnumerable<ISearchEngine> GetSearchEngines()
+        {
+            var engineTypes = System.Reflection.Assembly.GetExecutingAssembly().ExportedTypes
+                             .Where(t => t.GetInterfaces().Contains(typeof(ISearchEngine)));
+            List<ISearchEngine> searchEngines = new List<ISearchEngine>(); 
+            foreach (var type in engineTypes)
+            {
+                searchEngines.Add(type.GetConstructor(Type.EmptyTypes)?.Invoke(new object[0]) as ISearchEngine);
+            }
+            return searchEngines;
         }
     }
 }
